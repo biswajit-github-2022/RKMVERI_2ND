6de7d118a068c2090136554189e17d511c788812
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-01-03 20:23:27 +0530

    Lab

--- class/ETBDC/JAVA/hello.java
+++ class/ETBDC/JAVA/hello.java
@@ -1,5 +1,9 @@
+import java.util.*;
 class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(number);
     }
 }
