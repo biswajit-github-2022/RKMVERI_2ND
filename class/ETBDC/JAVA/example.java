1f2e4a7bf2cd73c31f47d22ee54f480bdbf6da43
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-01-05 14:33:09 +0530

    Lab

---
+++ class/ETBDC/JAVA/example.java
@@ -0,0 +1,14 @@
+public class example{
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(x);
+        example2 e= new example2();
+        int c=e.exp();
+        System.out.println(c);
+    }
+}
+class example2{
+    int exp(){
+        return 0;
+    }
+}
---
+++ class/ETBDC/JAVA/file_handle.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class file_handle {
+
+    public static void main(String[] args) {
+        String csvFile = "ODI_data.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            int column_number=3;
+            while ((line = br.readLine()) != null) {
+                // Split the CSV line into an array of values based on the comma
+                String[] values = line.split(",");
+                // System.out.println(Arrays.toString(values));
+                // Process the values as needed
+                // for (String value : values) {
+                //     System.out.print(value + " ");
+                // }
+                // System.out.println(); // Move to the next line for the next row
+                System.out.println(values[column_number]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
