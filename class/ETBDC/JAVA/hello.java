700aac8c87c4948cac29251be7ac9f0ca79c8838
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-01-05 14:33:09 +0530

    Lab

--- class/ETBDC/JAVA/hello.java
+++ class/ETBDC/JAVA/hello.java
@@ -1,10 +1,12 @@
 import java.util.*;
-class hello {
+class hi {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         System.out.println(number);
-        System.out.println()
     }
 }
+// java -d ../class_file hi.java
+//java -cp ../class_file hi 
+// jar cf MyApplication.jar -C classes/ .
